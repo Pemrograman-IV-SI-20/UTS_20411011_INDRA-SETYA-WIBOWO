219731d0339296eafd40de00fe6899f9431f9a1d
Indrasetya <indrasetyawibowo15@gmail.com>
2022-11-08 21:17:36 -0800

    uts

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:toko_gitar_flutter/Screens/Login/LoginScreens.dart';
+import 'package:toko_gitar_flutter/routes.dart';
+import 'package:toko_gitar_flutter/theme.dart';
+
+void main()async {
+  runApp(
+    MaterialApp(
+      title:  "Toko Guitar indra",
+      theme: theme(),
+      initialRoute: LoginScreen.routeName,
+      routes: routes,
+    ));
+}
